6e8ec294b13aa4e525e462ac38df025a27d06ee9
Gautam Kumar <gauta@amazon.com>
2023-08-04 00:15:35 -0700

    I think embedding insertion works

--- question_answer.py
+++ question_answer.py
@@ -39,10 +39,29 @@ chunks = split_text(transcript, 2048)
 
 # Initialize OpenAI and Pinecone
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+#Lets get all embedding in one shot 
+chunk_list = []
+for i in range(len(chunks)):
+  chunk_list.append(chunks[i])
+
+# Create embeddings for each chunk and store them in Pinecone
+res = openai.Embedding.create(input=chunk_list, engine="text-embedding-ada-002")
+embeds = [record['embedding'] for record in res['data']]
+
 pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
 
-index = pinecone.Index(index_name="my-podcast-index")
+index_name="my-podcast-index"
+# check if 'openai' index already exists (only create index if not)
+if index_name not in pinecone.list_indexes():
+    pinecone.create_index(index_name, dimension=len(embeds[0]))
 
+index = pinecone.Index(index_name=index_name)
+
+ids_batch = [str(n) for n in range(len(chunks)]
+meta = [{'text': line} for line in chunks]
+to_upsert = zip(ids_batch, embeds, meta)
+index.upsert(vectors=list(to_upsert))
 ##Lets get all embedding in one shot 
 #chunk_list = []
 #for i in range(len(chunks)):
@@ -51,13 +70,17 @@ index = pinecone.Index(index_name="my-podcast-index")
 ## Create embeddings for each chunk and store them in Pinecone
 #res = openai.Embedding.create(input=chunk_list, engine="text-embedding-ada-002")
 
-for i, chunk in enumerate(chunks):
-    res = openai.Embedding.create(
-        input=[chunk],
-        engine="text-embedding-ada-002"
-    )
-    vector = pinecone.Vector(res['data'][0]['embedding'], values=True)
-    index.upsert({f"podcast_chunk_{i}": vector})
+#for i, chunk in enumerate(chunks):
+#    res = openai.Embedding.create(
+#        input=[chunk],
+#        engine="text-embedding-ada-002"
+#    )
+#    print(type(res['data'][0]['embedding']))
+#    #vector = pinecone.Vector(res['data'][0]['embedding'], id=f"podcast_chunk_{i}")
+#    #vector = pinecone.Vector(res['data'][0]['embedding'], values=True)
+#    vectors = {f"podcast_chunk_{i}": res['data'][0]['embedding'] }
+#    index.upsert(vectors=vectors)
+#    break
 
 def answer_question(question):
     # Create an embedding for the question
@@ -67,7 +90,7 @@ def answer_question(question):
     )
 
     # Retrieve the most similar embeddings from Pinecone
-    res = index.query(pinecone.Vector(res['data'][0]['embedding']), top_k=1, include_metadata=True)
+    res = index.query([res['data'][0]['embedding'], top_k=1, include_metadata=True)
     
     # Construct the prompt for the generative model
     prompt = f"Context: {res['matches'][0]['id']}\nQuestion: {question}\nAnswer:"
