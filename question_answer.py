99237f598bd046529fb1fe7105cbb1cfe417ae00
Gautam Kumar <gauta@amazon.com>
2023-08-10 23:40:23 -0700

    Adding Q & A

--- question_answer.py
+++ question_answer.py
@@ -2,18 +2,27 @@ import os
 import openai
 import pinecone
 import tiktoken
+import logging
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Constants
+MAX_TOKENS_PER_CHUNK = 2048
+
+# Initialize APIs and keys
+openai.api_key = os.getenv('OPENAI_API_KEY')
+pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
+index_name = "my-podcast-index"
 
 # Initialize the tokenizer
-
 def count_tokens(text):
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.get_encoding("gpt2")  # Use gpt2 encoding for token counting
+    encoding = tiktoken.get_encoding("gpt2")
     num_tokens = len(encoding.encode(text))
     return num_tokens
 
 def split_text(text, max_tokens):
-    """Splits the text into chunks that have fewer than max_tokens each."""
     words = text.split()
     chunks = []
     chunk = []
@@ -26,77 +35,85 @@ def split_text(text, max_tokens):
             num_tokens = 0
         chunk.append(word)
         num_tokens += word_tokens
-    chunks.append(' '.join(chunk))  # Don't forget the last chunk
+    chunks.append(' '.join(chunk))
     return chunks
 
+def initialize_pinecone_index():
+    if index_name not in pinecone.list_indexes():
+        pinecone.create_index(index_name, dimension=len(embeds[0]))
+        logger.info("Pinecone index '%s' created.", index_name)
 
-# Load your podcast transcript data from a file
-with open('/Users/gauta/Downloads/podcasts/transcripts/httpslexfridman.comgeorge-hotz-3-transcript.txt', 'r') as file:
-    transcript = file.read()
+def get_podcast_transcript_chunks():
+    with open('/Users/gauta/Downloads/podcasts/transcripts/httpslexfridman.comgeorge-hotz-3-transcript.txt', 'r') as file:
+        transcript = file.read()
+    return split_text(transcript, MAX_TOKENS_PER_CHUNK)
 
-# Split the transcript into chunks of 2048 tokens each
-chunks = split_text(transcript, 2048)
+def store_embeddings_in_pinecone(embeds, chunks):
+    index = pinecone.Index(index_name=index_name)
+    ids_batch = [str(n) for n in range(len(chunks))]
+    meta = [{'text': line} for line in chunks]
+    to_upsert = zip(ids_batch, embeds, meta)
+    index.upsert(vectors=list(to_upsert))
+    logger.info("Embeddings stored in Pinecone index '%s'.", index_name)
 
-# Initialize OpenAI and Pinecone
-openai.api_key = os.getenv('OPENAI_API_KEY')
+# TODO: Store the embedding and pinecone storage details 
+# Avoid calling the embedding generation and storage twice
+def get_or_create_embeddings():
+    chunks = get_podcast_transcript_chunks()
 
-#Lets get all embedding in one shot 
-chunk_list = []
-for i in range(len(chunks)):
-  chunk_list.append(chunks[i])
+    # Check if embeddings have already been created and stored
+    if embeddings_exist_in_storage():
+        logger.info("Embeddings already exist. Skipping creation and storage.")
+        return load_embeddings_from_storage()
 
-# Create embeddings for each chunk and store them in Pinecone
-res = openai.Embedding.create(input=chunk_list, engine="text-embedding-ada-002")
-embeds = [record['embedding'] for record in res['data']]
-
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
-
-index_name="my-podcast-index"
-# check if index already exists (only create index if not)
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(index_name, dimension=len(embeds[0]))
-
-index = pinecone.Index(index_name=index_name)
-
-ids_batch = [str(n) for n in range(len(chunks))]
-meta = [{'text': line} for line in chunks]
-to_upsert = zip(ids_batch, embeds, meta)
-index.upsert(vectors=list(to_upsert))
-
-def answer_question(question):
-    # Create an embedding for the question
-    res = openai.Embedding.create(
-        input=[question],
-        engine="text-embedding-ada-002"
-    )
-
-    # Retrieve the most similar embeddings from Pinecone
-    res = index.query([res['data'][0]['embedding']], top_k=1, include_metadata=True)
+    chunk_list = chunks[:]
+    res = openai.Embedding.create(input=chunk_list, engine="text-embedding-ada-002")
+    embeds = [record['embedding'] for record in res['data']]
     
-    # Construct the prompt for the generative model
-    prompt = f"Context: {res['matches'][0]['id']}\nQuestion: {question}\nAnswer:"
+    initialize_pinecone_index()
+    store_embeddings_in_pinecone(embeds, chunks)
+    return chunks, embeds
 
-    # Generate an answer to the question
+def embeddings_exist_in_storage():
+    # Implement your logic here to check if embeddings are already stored
+    return False
+
+def load_embeddings_from_storage():
+    # Implement your logic here to load and return embeddings from storage
+    return [], []
+
+def get_answer_from_generative_model(question, index):
+    res = openai.Embedding.create(input=[question], engine="text-embedding-ada-002")
+    res = index.query([res['data'][0]['embedding']], top_k=1, include_metadata=True)
+    prompt = f"Context: {res['matches'][0]['id']}\nQuestion: {question}\nAnswer:"
     res = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         temperature=0,
         max_tokens=60,
     )
-
     return res['choices'][0]['text'].strip()
 
+# Main execution
+def main():
+    chunks, embeds = get_or_create_embeddings()
 
-while True:
-    try:
-        user_query = input("Enter your query (or type 'quit' to exit): ")
-        if user_query.lower() == 'quit':
-            print("Exiting...")
+    index = pinecone.Index(index_name=index_name)
+
+    while True:
+        try:
+            user_query = input("Enter your query (or type 'quit' to exit): ")
+            if user_query.lower() == 'quit':
+                print("Exiting...")
+                break
+
+            answer = get_answer_from_generative_model(user_query, index)
+            print("Answer:", answer)
+
+        except KeyboardInterrupt:
+            print("\nExiting...")
             break
-        
-        answer = answer_question(user_query)
-        print("Answer:", answer)
-        
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        break
+
+if __name__ == "__main__":
+    main()
+
